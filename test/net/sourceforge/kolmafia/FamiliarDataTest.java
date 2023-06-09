91af59517eb8bf77155320faff5808d73024ec77
edac99 <jamjam1208@naver.com>
2023-06-09 17:31:46 +0900

    test: Test for familiar that find item
    Write a test to see if a familiar can find a specific rare item.

--- test/net/sourceforge/kolmafia/FamiliarDataTest.java
+++ test/net/sourceforge/kolmafia/FamiliarDataTest.java
@@ -2,16 +2,8 @@ package net.sourceforge.kolmafia;
 
 import static internal.helpers.Networking.html;
 import static internal.helpers.Networking.json;
-import static internal.helpers.Player.withClass;
-import static internal.helpers.Player.withEffect;
-import static internal.helpers.Player.withEquipped;
-import static internal.helpers.Player.withFamiliar;
-import static internal.helpers.Player.withNotAllowedInStandard;
-import static internal.helpers.Player.withPath;
-import static internal.helpers.Player.withProperty;
-import static internal.helpers.Player.withRestricted;
-import static internal.helpers.Player.withSkill;
-import static internal.helpers.Player.withStats;
+import static internal.helpers.Player.*;
+import static internal.helpers.Player.withFamiliarInTerrariumWithItem;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -222,6 +214,16 @@ public class FamiliarDataTest {
     }
   }
 
+  @Test
+  public void findGoodItemTest() {
+    var familiar = FamiliarData.registerFamiliar(FamiliarPool.MOSQUITO, 0);
+    AdventureResult item = ItemPool.get(ItemPool.LEAD_NECKLACE, 1);
+    familiar.setItem(item);
+    KoLCharacter.addFamiliar(familiar);
+
+    AdventureResult result = familiar.findGoodItem(true);
+    assertEquals(familiar.LEAD_NECKLACE, result);
+  }
   @Nested
   class Terrarium {
     @BeforeAll
