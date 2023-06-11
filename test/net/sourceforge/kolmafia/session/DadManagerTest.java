8ba0ef5415137586902b9890d69ebe2084b0d5ea
edac99 <jamjam1208@naver.com>
2023-06-11 18:39:16 +0900

    test: Test for expected weakness for a particular clue
    Set 10 clues in the script.
    Testing whether the weaknesses computed for this are correct.

--- test/net/sourceforge/kolmafia/session/DadManagerTest.java
+++ test/net/sourceforge/kolmafia/session/DadManagerTest.java
@@ -16,6 +16,34 @@ public class DadManagerTest {
     Preferences.reset("DadManagerTest");
   }
 
+  private String setResponseText(String[] clueTextList) {
+    return "You shake your head and look above the tank, at the window into space. "
+        + clueTextList[1]
+        + " forms "
+        + clueTextList[2]
+        + " in the darkness, each more "
+        + clueTextList[3]
+        + " than the last. "
+        + "The "
+        + clueTextList[4]
+        + " "
+        + clueTextList[5]
+        + ", "
+        + clueTextList[6]
+        + " revealing "
+        + clueTextList[7]
+        + "-dimensional monstrosities.. No. "
+        + "Look again. There is nothing. Are your "
+        + clueTextList[8]
+        + " betraying you? "
+        + "As if on cue, "
+        + clueTextList[9]
+        + "-sided triangles materialize and then disappear. "
+        + "So impossible that your "
+        + clueTextList[10]
+        + " throbs.";
+  }
+
   @Test
   public void canSearchElementName() {
     Element validElement = Element.SLEAZE;
@@ -28,6 +56,44 @@ public class DadManagerTest {
     Element validElement = Element.SLEAZE;
     String elementSpell = DadManager.elementToSpell(validElement);
     assertEquals("Unknown", elementSpell);
+  }
+
+  @Test
+  public void canSetAllWeaknessWhenValidResponseText() {
+    String[] clueTextList = {
+      "",
+      "horrifying",
+      "swim",
+      "awful",
+      "space",
+      "cracks open",
+      "suddenly",
+      "32",
+      "eyes",
+      "3",
+      "stomach"
+    };
+    String validResponseText = setResponseText(clueTextList);
+    Element[] weaknessList = {
+      Element.NONE,
+      Element.SPOOKY,
+      Element.PHYSICAL,
+      Element.COLD,
+      Element.PHYSICAL,
+      Element.COLD,
+      Element.SLEAZE,
+      Element.SLEAZE,
+      Element.SPOOKY,
+      Element.NONE,
+      Element.PHYSICAL
+    };
+
+    DadManager.solve(validResponseText);
+
+    int index = 0;
+    for (DadManager.Element elementalWeakness : DadManager.ElementalWeakness) {
+      assertEquals(weaknessList[index++], elementalWeakness);
+    }
   }
 
 }
